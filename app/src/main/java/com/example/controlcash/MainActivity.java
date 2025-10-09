88dfc8fc7a33f1f313d50039e48f6f0cbb8874cd
Mikeyloco1973 <ionuevolpep@gmail.com>
2025-10-09 14:50:08 -0300

    Ejemplo

--- app/src/main/java/com/example/controlcash/MainActivity.java
+++ app/src/main/java/com/example/controlcash/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.controlcash;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this, "¡Alerta mostrada con Toast!", Toast.LENGTH_SHORT).show();
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
